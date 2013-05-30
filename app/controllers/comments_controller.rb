da65d5e0db8cda9d83dd744080be47c3bf9183af
Jon Lue <tbonesteak@gmail.com>
2013-05-29 20:01:43 -0500

    Everything should be working properly

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,8 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    #@comments = Comment.all
+    @comments = Comment.where(:post_id => params[:post_id])
 
     respond_to do |format|
       format.html # index.html.erb
@@ -38,12 +39,11 @@ class CommentsController < ApplicationController
   # GET /comments/1/edit
   def edit
     #@comment = current_user.comments.find(params[:id])
+    @admin = user_signed_in?&&current_user.admin?
     @comment = Comment.find(params[:id])
-    if
-    @comment.user == current_user
+    if @comment.user == current_user || @admin
       render "edit"
-    else
-      flash[:alert] = "you can't edit other peoples blah blah blah"
+    else flash[:alert] = "You do not have permission to do that."
       redirect_to :back
     end
   end
@@ -58,7 +58,8 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        #format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to :back, notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render action: "new" }
@@ -71,26 +72,34 @@ class CommentsController < ApplicationController
   # PUT /comments/1.json
   def update
     #@comment = current_user.comments.find(params[:id])
+    @admin = user_signed_in?&&current_user.admin?
     @comment = Comment.find(params[:id])
-    @comment.user == current_user
-
+    if @comment.user == current_user || @admin
     respond_to do |format|
       if @comment.update_attributes(params[:comment])
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        #format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to :back, notice: 'Comment was successfully created.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
+    else flash[:alert] = "You do not have permission to do that."
+      redirect_to :back
+    end
+    end
   end
 
   # DELETE /comments/1
   # DELETE /comments/1.json
   def destroy
+    @admin = user_signed_in?&&current_user.admin?
     @comment = current_user.comments.find(params[:id])
-    @comment.destroy
-    @comment.user == current_user
+    if @comment.user == current_user || @admin
+      @comment.destroy
+    else flash[:alert] = "You do not have permission to do that."
+      redirect_to :back
 
     respond_to do |format|
       format.html { redirect_to comments_url }
